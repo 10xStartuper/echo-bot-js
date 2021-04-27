8576cfeec07663c9d4b4b6a3009fee8fec9064c4
Mukhammadyusuf <icoderx@yandex.ru>
2021-04-27 15:25:22 +0500

    Echo bot is rady to use

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const { bot, logMessage } = require("./index");
 
+const helpMessage = `
+Say thomething to me:
+/start - starts the bot
+/help - command refference
+/echo - echo command
+/echo [Text argument]
+`;
+
 bot.use((ctx, next) => {
   logMessage(ctx);
   next();
 });
+bot.start((ctx) => {
+  ctx.reply("Assalomu alaykum botimizga xush kelibsiz!");
+  ctx.reply(helpMessage);
+});
+bot.help((ctx) => {
+  ctx.reply(helpMessage);
+});
+bot.command("echo", (ctx) => {
+  let input = ctx.message.text;
+  let inputArray = input.split(" ");
 
+  let message = "";
+
+  if (inputArray.length == 1) {
+    message = "You said echo";
+  } else {
+    inputArray.shift();
+    message = inputArray.join(" ");
+  }
+
+  ctx.reply(message);
+});
 bot.launch();
